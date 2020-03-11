cf92d52bc98711171839e80c080cd17f7232a3a6
ogata216 <amidstasinglebreath@gmail.com>
2020-03-10 23:46:18 -0700

    reference error fixes

--- client/src/components/pages/Search/index.js
+++ client/src/components/pages/Search/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import API from "../../utils/API";
+import API from "../../../utils/API";
 import SearchBar from "../../SearchBar";
 import Book from "../../Book";
 import { Container } from "react-bootstrap";
